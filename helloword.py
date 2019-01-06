4da2a54a30f0270b444ba8622b8290cd5df0e799
Fredrik Sloberg <30707124+fsberg@users.noreply.github.com>
2019-01-06 01:28:19 +0100

    2

--- helloword.py
+++ helloword.py
@@ -1,7 +1,14 @@
+num_ls = 20
+
 print("hello world")
 x = input("?")
 print(x)
 
 for i in range(9):
     print(i)
-    
+
+if( num_ls == 20):
+    print("hej\n")
+    num_ls -= num_ls
+else:
+    print("slut")
